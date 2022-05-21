d4a997949ac57a37b78faf1b7146ed097560822b
Emanuele Ballarin <emanuele@ballarin.cc>
2022-05-21 11:38:24 +0200

    Fixes...

--- load.ps1
+++ load.ps1
@@ -3,12 +3,14 @@ scp *.png remarkable:/usr/share/remarkable/templates/
 scp *.svg remarkable:/usr/share/remarkable/templates/
 
 # Copy original templates.json
+Remove-Item -R -Force ./templates.original.json
 scp remarkable:/usr/share/remarkable/templates/templates.json ./templates.original.json
 
 # Backup original templates.json on-device
-ssh remarkable "cp /usr/share/remarkable/templates/templates.json /usr/share/remarkable/templates/templates.original.json"
+ssh remarkable "mv /usr/share/remarkable/templates/templates.json /usr/share/remarkable/templates/templates.original.json"
 
 # Merge jsons (new = original + additions)
+Remove-Item -R -Force ./templates.merged.json
 jq -n '{ templates: [ inputs.templates ] | add }' templates.original.json templates.addition.json > templates.merged.json
 
 # Copy new templates.json
